ed4c64871951a4cca8ee43b8036093e64f295b55
JulnarAssi <1221669@student.birzeit.edu>
2026-06-23 11:59:38 +0300

    Create PID controller module

---
+++ code/pid_controller.py
@@ -0,0 +1,27 @@
+# PID Controller
+
+class PIDController:
+
+    def __init__(self, kp, ki, kd):
+        self.kp = kp
+        self.ki = ki
+        self.kd = kd
+
+        self.previous_error = 0
+        self.integral = 0
+
+    def calculate(self, error):
+
+        self.integral += error
+
+        derivative = error - self.previous_error
+
+        output = (
+            self.kp * error +
+            self.ki * self.integral +
+            self.kd * derivative
+        )
+
+        self.previous_error = error
+
+        return output
