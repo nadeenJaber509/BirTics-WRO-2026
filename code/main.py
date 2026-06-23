6c646ef49492451f1bbf0a693f99893a8273c349
JulnarAssi <1221669@student.birzeit.edu>
2026-06-23 11:54:53 +0300

    Create initial robot software structure

---
+++ code/main.py
@@ -0,0 +1,35 @@
+# Main Robot Program
+
+def initialize_system():
+    pass
+
+def read_sensors():
+    pass
+
+def lane_following():
+    pass
+
+def detect_pillar():
+    pass
+
+def avoid_pillar():
+    pass
+
+def count_laps():
+    pass
+
+def parking():
+    pass
+
+def stop_robot():
+    pass
+
+def main():
+    initialize_system()
+
+    while True:
+        read_sensors()
+        lane_following()
+
+if __name__ == "__main__":
+    main()
