db4f83ac3cc7999ff2ba156fbe740a3907e92baa
JulnarAssi <1221669@student.birzeit.edu>
2026-06-24 17:48:42 +0300

    Update sensor module for three LiDAR sensors

--- code/sensors.py
+++ code/sensors.py
@@ -1,9 +1,18 @@
 # Sensor Module
 
-def read_tof_left():
+def select_multiplexer_channel(channel):
     pass
 
-def read_tof_right():
+def read_lidar_left():
+    select_multiplexer_channel(0)
+    pass
+
+def read_lidar_middle():
+    select_multiplexer_channel(1)
+    pass
+
+def read_lidar_right():
+    select_multiplexer_channel(2)
     pass
 
 def read_color_left():
@@ -20,8 +29,9 @@ def read_ir_sensor():
 
 def read_all_sensors():
     return {
-        "tof_left": read_tof_left(),
-        "tof_right": read_tof_right(),
+        "lidar_left": read_lidar_left(),
+        "lidar_middle": read_lidar_middle(),
+        "lidar_right": read_lidar_right(),
         "color_left": read_color_left(),
         "color_right": read_color_right(),
         "imu": read_imu(),
