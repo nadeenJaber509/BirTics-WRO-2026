4c9c3791583eed7f9e4c9f528bbaffa9e469f78a
JulnarAssi <1221669@student.birzeit.edu>
2026-07-19 01:28:19 +0300

    Replace pillar and parking states with lane following and obstacle avoidance states

--- code/design/state_machine.py
+++ code/design/state_machine.py
@@ -1,13 +1,11 @@
 # Robot State Machine
 
-from enum import Enum
+from enum import Enum, auto
 
 
 class RobotState(Enum):
-    IDLE_WAIT_START = 0
-    LANE_FOLLOW = 1
-    PILLAR_AVOID_RED_RIGHT = 2
-    PILLAR_AVOID_GREEN_LEFT = 3
-    LAP_COUNT_FINISH = 4
-    PARKING = 5
-    STOP = 6
+    IDLE = auto()
+    WAIT_FOR_START = auto()
+    LANE_FOLLOWING = auto()
+    OBSTACLE_AVOIDANCE = auto()
+    MISSION_COMPLETE = auto()
